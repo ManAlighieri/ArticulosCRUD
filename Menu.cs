ff6bd64e4c389534e97a74e163d9bf3ec6abb06b
MonAlighieri <dega2005alej@gmail.com>
2026-08-27 11:18:33 -0600

    Menu estatico

---
+++ Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticulosCRUD
+{
+    internal class Menu
+    {
+        public Menu()
+        {
+
+        }
+
+        public void MostrarMenu()
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.Clear();
+                Console.WriteLine("Gestor de Articulos");
+                Console.WriteLine("===================");
+                Console.WriteLine("1. Agregar");
+                Console.WriteLine("2. Listar");
+                Console.WriteLine("3. Buscar");
+                Console.WriteLine("4. Modificar");
+                Console.WriteLine("5. Eliminar");
+                Console.WriteLine("0. Salir");
+                string option = Console.ReadLine() ?? "";
+                switch (option)
+                {
+                    case "0":
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion Invalidad");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
