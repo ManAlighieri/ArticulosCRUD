d9540df97e38d70cf07345a3aeb1e44fb4001d2e
MonAlighieri <dega2005alej@gmail.com>
2026-09-01 11:42:13 -0600

    Agregar Productos parcial

--- Menu.cs
+++ Menu.cs
@@ -8,10 +8,12 @@ namespace ArticulosCRUD
     {
         private readonly string Titulo;
         private readonly string[] Opciones;
+        private List<Producto> ListaProductos;
         public Menu(string titulo, string[] opciones)
         {
             Titulo = titulo;
             Opciones = opciones;
+            ListaProductos = new List<Producto>();
         }
 
         public void MostrarMenu()
@@ -67,13 +69,29 @@ namespace ArticulosCRUD
         public void MostrarAgregar()
         {
             Console.Clear();
-            Console.WriteLine("Opcion Agregar Seleccionada");
+            Console.WriteLine("Agregar Producto");
+            Console.WriteLine("=================");
+            Console.WriteLine();
+            Console.WriteLine("Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Precio: ");
+            decimal precio = (decimal.TryParse(Console.ReadLine(),  out decimal valor)) ? valor : 0;
+            Console.WriteLine("Cantidad: ");
+            int cantidad = (int.TryParse(Console.ReadLine(), out int valor2)) ? valor2 : 0;
+            Producto producto = new Producto(ListaProductos.Count() + 1, nombre, cantidad, precio);
+            ListaProductos.Add(producto);
+            Console.WriteLine("Producto creado correctamente");
             Console.ReadLine();
         }
         public void MostrarListar()
         {
             Console.Clear();
-            Console.WriteLine("Opcion Listar Seleccionada");
+            Console.WriteLine("Listar Productos");
+            Console.WriteLine("=================");
+            foreach (Producto item in ListaProductos) 
+            { 
+                Console.WriteLine(item.Nombre); 
+            }
             Console.ReadLine();
         }
         public void MostrarBuscar()
--- Producto.cs
+++ Producto.cs
@@ -6,5 +6,17 @@ namespace ArticulosCRUD
 {
     internal class Producto
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+        public Producto(int id, string nombre, int cantidad, decimal precio) 
+        { 
+            Id = id;
+            Nombre = nombre;
+            Cantidad = cantidad;
+            Precio = precio;
+        }
     }
 }
